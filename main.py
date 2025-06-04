712e2b669025aa71745ebb9f5cab4b2873e319de
MatthewA570 <maa3833@rit.edu>
2025-06-04 16:15:49 -0400

    Implementation of edit transaction functionality using CLI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from database import initialize_database
 from database import new_Connection
+from database import modifiable_attributes
 import models.models
 
 def add_transaction():
@@ -21,21 +22,64 @@ def add_transaction():
     #TODO: fix order of attributes in constructor and user prompts to match sql table
     connect = new_Connection()
     cursor = connect.cursor()
-    cursor.execute("INSERT INTO record_book VALUES(name, category, cost, date)")
+    cursor.execute("INSERT INTO record_book (NAME, CATEGORY, COST, DATE) VALUES (?, ?, ?, ?)", (name, category, cost, date))
+    connect.commit()
     cursor.close()
     connect.close()
 
 
+
 def edit_transaction():
-    givenID = input("Please provide id of transaction you wish to edit: ")
+    given_ID = input("Please provide id of transaction you wish to edit: ")
     connect = new_Connection()
     cursor = connect.cursor()
-        
+    cursor.execute("SELECT * FROM record_book WHERE ID = ?", (given_ID))
+    row = cursor.fetchone()
+    if row is None:
+        print("A transaction with that ID does not exist. Please provide an existing ID")
+    else:
+        print("Transaction found! ")
+        while True:
+            print("1) Name      2) Category      3) Cost      4) Date       5) Quit")
+            attribute = input("Please select a number from the list above. If no more edits requested, please type 5: ")
+            if attribute == "5":
+                return False
+            while attribute not in ["1", "2", "3", "4", "5"]:
+                print("1) Name      2) Category      3) Cost      4) Date       5) Quit\n")
+                attribute = input("Please select a number from the list above. If no more edits requested, please type 5: ")
+
+            match attribute:
+                case "1":
+                    new_name = input("What would you like new name to be?: ")
+                    cursor.execute("UPDATE record_book SET NAME = ? WHERE ID = ?", (new_name, given_ID))
+                    connect.commit()
+                case "2":
+                    new_cat = input("What would you like new category to be?: ")
+                    print(f"Available categories: {models.models.categories}")
+                    cursor.execute("UPDATE record_book SET CATEGORY = ? WHERE ID = ?", (new_cat, given_ID))
+                    connect.commit()
+                case "3":
+                    new_cost = input("What would you like new cost to be?: ")
+                    cursor.execute("UPDATE record_book SET COST = ? WHERE ID = ?", (new_cost, given_ID))
+                    connect.commit()
+                case "4":
+                    new_date = input("What would you like new date to be?: ")
+                    cursor.execute("UPDATE record_book SET DATE = ? WHERE ID = ?", (new_date, given_ID))
+                    connect.commit()
+                case "5":
+                    return False
+        #END OF WHILE
+    connect.commit()
+    cursor.close()
+    connect.close()
 
 
 def main():
     #Create new table record book if not existent 
     initialize_database()
+    #add_transaction()
+    edit_transaction()
 
 
+main()
     
